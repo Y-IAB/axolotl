2570b8f93f98293f93fe779e2df3c95026cf5ab9
Rifqi Alfi/IAB(YA) <rifqi.alfi@yanolja.com>
2024-03-28 15:17:02 +0700

    add debug print

--- src/axolotl/utils/vessl_.py
+++ src/axolotl/utils/vessl_.py
@@ -9,6 +9,7 @@ def setup_vessl_env_vars(cfg: DictDefault):
     # VESSL_RUN_INITIAL_CONFIG is a variable that contain path to
     # default credential inside a VESSL Run
     credential_path = os.environ.get("VESSL_RUN_INITIAL_CONFIG")
+    print(cfg.use_vessl)
     if credential_path:
         cfg.use_vessl = True
         cfg.vessl_credential_path = credential_path

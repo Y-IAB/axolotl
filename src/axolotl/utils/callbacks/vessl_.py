b917faaf401f7ab048bf6f1b99dbbc3bd3fd8e79
Rifqi Alfi/IAB(YA) <rifqi.alfi@yanolja.com>
2024-03-28 15:15:56 +0700

    remove metrics from constructor

--- src/axolotl/utils/callbacks/vessl_.py
+++ src/axolotl/utils/callbacks/vessl_.py
@@ -10,7 +10,7 @@ LOG = logging.getLogger("axolotl.callbacks")
 
 class VesslLogMetricsCallback(TrainerCallback):
 
-    def __init__(self, credential_path: str, metrics: List[str]) -> None:
+    def __init__(self, credential_path: str) -> None:
         vessl.configure(credentials_file=credential_path)
 
     def on_log(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, logs: Dict[str, float] = None, **kwargs):     
